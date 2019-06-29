c72dd758c08f47aaec5a63eceea35f9747c68100
Radha Jain <jainr@stanford.edu>
2019-06-29 12:46:01 -0700

    Analytics fully working

--- src/components/Analytics/index.js
+++ src/components/Analytics/index.js
@@ -14,6 +14,10 @@ class Analytics extends React.Component {
         super(props);
         this.state = {
             measurements: null,
+            heights: null,
+            waist: null,
+            hips: null,
+            bust: null,
         }
         this.addNamesToReviews = this.addNamesToReviews.bind(this);
         this.getMeasurements = this.getMeasurements.bind(this);
@@ -23,11 +27,16 @@ class Analytics extends React.Component {
     componentDidMount() {
         // Run this function when we need to add names to reviews
         // this.addNamesToReviews();
-        this.getMeasurements().then((mmt) => {
+        this.getMeasurements().then((mmts) => {
             this.setState({
-                measurements: mmt
+                measurements: mmts[0],
+                heights: mmts[1],
+                waist: mmts[2],
+                hips: mmts[3],
+                bust: mmts[4]
             });
-            console.log(this.state.measurements);
+            console.log(this.state);
+            
         });
         
     }
@@ -56,16 +65,108 @@ class Analytics extends React.Component {
         var measurementsRef = firebase.database().ref('measurements');
         return measurementsRef.once('value').then((snapshot) => {
             var measurements = []
+            var heights =  {
+                60: 0,
+                61: 0,
+                62: 0,
+                63: 0,
+                64: 0,
+                65: 0,
+                66: 0,
+                66.5: 0,
+                67: 0,
+                68: 0,
+                69: 0,
+                70: 0,
+                71: 0,
+                72: 0,
+                73: 0,
+                74: 0,
+            };
+            var waist = {
+                22: 0,
+                23: 0,
+                24: 0,
+                25: 0,
+                25.5: 0,
+                26: 0,
+                27: 0,
+                28: 0,
+                28.5: 0,
+                29: 0,
+                30: 0,
+                31: 0,
+                32: 0,
+                33: 0,
+                34: 0,
+                35: 0,
+                35.5: 0,
+                36: 0,
+                37: 0,
+                38: 0,
+                39: 0,
+                40: 0,
+                41: 0,
+                42: 0
+            }
+
+            var hips = {
+                22.5: 0,
+                27: 0,
+                32: 0,
+                33: 0,
+                34: 0,
+                35: 0,
+                35.5 : 0,
+                36: 0,
+                37: 0,
+                38: 0,
+                38.5: 0,
+                39: 0,
+                40: 0,
+                41: 0,
+                41.5: 0,
+                42: 0,
+                43: 0,
+                44: 0,
+                45: 0,
+                47: 0,
+                49: 0,
+                51: 0
+            }
+            var bust = {
+                32: 0,
+            }
             snapshot.forEach((child) => {
                 var mmt = {}
                 var concatMtms = child.val().concatMtms;
+                var waistMt = child.val().waist;
+                var newWaist = waist[waistMt] + 1;
+                waist[waistMt] = newWaist;
+                var hipsMt = child.val().hips;
+                var newHips = hips[hipsMt] + 1;
+                hips[hipsMt] = newHips;
+                var heightMt = child.val().height;
+                var newHeight = heights[heightMt] + 1;
+                heights[heightMt] = newHeight;
+                heights[heightMt] = newHeight;
+                var bustMt = child.val().bust;
+                if (bustMt > 300) {
+                    console.log(child.key);
+                }
+                if (bust[bustMt] === undefined) {
+                    bust[bustMt] = 1
+                } else {
+                    var newBust = bust[bustMt] + 1;
+                    bust[bustMt] = newBust;
+                }
                 this.getNumChildren(child.val().dressGroupID).then(numChildren => {
                     mmt.children = numChildren;
                     mmt.measurement = concatMtms;
                     measurements.push(mmt);
                 });
             });
-            return measurements;
+            return [measurements, heights, waist, hips, bust];
         });
     }
 
@@ -76,14 +177,14 @@ class Analytics extends React.Component {
         return (
             <div>
                 <p>Analytics</p>
-                {this.state.measurements && this.state.measurements.map((measurement, key) => {
+                {/* {this.state.measurements && this.state.measurements.map((measurement, key) => {
                     return (
                         <div key={key}>
                             <p>{measurement.measurement}</p>
                             <p>{measurement.children}</p>
                         </div>
                     );
-                })}
+                })} */}
             </div> 
         );
     }
