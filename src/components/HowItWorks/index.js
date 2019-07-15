52cef359a4cc36cfe37c793a061e70384c9f7cea
Radha Jain <jainr@stanford.edu>
2019-07-15 15:21:50 -0700

    increasing speed

--- src/components/HowItWorks/index.js
+++ src/components/HowItWorks/index.js
@@ -20,7 +20,7 @@ class HowItWorks extends React.Component {
             <div className="howitworks-content">
               <div className="howitworks-title-div">
                 <div className="howitworks-titleCol">
-                  <p className="howitworks-title">Shopping, as it should be.</p>
+                  <p className="howitworks-title">Shopping, based on your body.</p>
                 </div>
                 <div className="howitworks-titleCol">
                   <p className="howitworks-title-desc">
