dce2c0430c2ea9afbf6e5328db8477650844033e
Radha Jain <jainr@stanford.edu>
2019-07-16 11:36:09 -0700

    with profile dropdown

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -115,7 +115,7 @@ class AccountPage extends React.Component {
               <form>
                 <div className="account-section-wrapper account-section-wrapper-top">
                   <div className="account-wrapper-item">
-                    <div className="account-select-round">
+                    <span>
                       <select
                         name="height"
                         type="select"
@@ -144,153 +144,145 @@ class AccountPage extends React.Component {
                         <option value="77">6' 5"</option>
                         <option value="78">6' 6"</option>
                       </select>
-                    </div>
+                    </span>
                     <label className="account-item-label">Height</label>
                   </div>
                   <div className="account-wrapper-item">
-                    <div className="account-select-round">
-                      <select
-                        value={this.state.bra}
-                        onChange={this.handleChange('bra')}
-                        name="bra"
-                        className="account-select account-lg-input"
-                      >
-                        <option value="30AA">30AA</option>
-                        <option value="30A">30A</option>
-                        <option value="30B">30B</option>
-                        <option value="30C">30C</option>
-                        <option value="30D">30D</option>
-                        <option value="30DD">30DD</option>
-                        <option value="30DDD/E">30E</option>
-                        <option value="30F">30F</option>
-                        <option value="30G">30G</option>
-                        <option value="30H">30H</option>
-                        <option value="30I">30I</option>
-                        <option value="30J">30J</option>
-                        <option value="32AA">32AA</option>
-                        <option value="32A">32A</option>
-                        <option value="32B">32B</option>
-                        <option value="32C">32C</option>
-                        <option value="32D">32D</option>
-                        <option value="32DD">32DD</option>
-                        <option value="32DDD/E">32E</option>
-                        <option value="32F">32F</option>
-                        <option value="32G">32G</option>
-                        <option value="32H">32H</option>
-                        <option value="32I">32I</option>
-                        <option value="32J">32J</option>
-                        <option value="34AA">34AA</option>
-                        <option value="34A">34A</option>
-                        <option value="34B">34B</option>
-                        <option value="34C">34C</option>
-                        <option value="34D">34D</option>
-                        <option value="34DD">34DD</option>
-                        <option value="34DDD/E">34E</option>
-                        <option value="34F">34F</option>
-                        <option value="34G">34G</option>
-                        <option value="34H">34H</option>
-                        <option value="34I">34I</option>
-                        <option value="34J">34J</option>
-                        <option value="36AA">36AA</option>
-                        <option value="36A">36A</option>
-                        <option value="36B">36B</option>
-                        <option value="36C">36C</option>
-                        <option value="36D">36D</option>
-                        <option value="36DD">36DD</option>
-                        <option value="36DDD/E">36E</option>
-                        <option value="36F">36F</option>
-                        <option value="36G">36G</option>
-                        <option value="36H">36H</option>
-                        <option value="36I">36I</option>
-                        <option value="36J">36J</option>
-                        <option value="38AA">38AA</option>
-                        <option value="38A">38A</option>
-                        <option value="38B">38B</option>
-                        <option value="38C">38C</option>
-                        <option value="38D">38D</option>
-                        <option value="38DD">38DD</option>
-                        <option value="38DDD/E">38E</option>
-                        <option value="38F">38F</option>
-                        <option value="38G">38G</option>
-                        <option value="38H">38H</option>
-                        <option value="38I">38I</option>
-                        <option value="38J">38J</option>
-                      </select>
-                    </div>
+                    <select
+                      value={this.state.bra}
+                      onChange={this.handleChange('bra')}
+                      name="bra"
+                      className="account-select account-lg-input"
+                    >
+                      <option value="30AA">30AA</option>
+                      <option value="30A">30A</option>
+                      <option value="30B">30B</option>
+                      <option value="30C">30C</option>
+                      <option value="30D">30D</option>
+                      <option value="30DD">30DD</option>
+                      <option value="30DDD/E">30E</option>
+                      <option value="30F">30F</option>
+                      <option value="30G">30G</option>
+                      <option value="30H">30H</option>
+                      <option value="30I">30I</option>
+                      <option value="30J">30J</option>
+                      <option value="32AA">32AA</option>
+                      <option value="32A">32A</option>
+                      <option value="32B">32B</option>
+                      <option value="32C">32C</option>
+                      <option value="32D">32D</option>
+                      <option value="32DD">32DD</option>
+                      <option value="32DDD/E">32E</option>
+                      <option value="32F">32F</option>
+                      <option value="32G">32G</option>
+                      <option value="32H">32H</option>
+                      <option value="32I">32I</option>
+                      <option value="32J">32J</option>
+                      <option value="34AA">34AA</option>
+                      <option value="34A">34A</option>
+                      <option value="34B">34B</option>
+                      <option value="34C">34C</option>
+                      <option value="34D">34D</option>
+                      <option value="34DD">34DD</option>
+                      <option value="34DDD/E">34E</option>
+                      <option value="34F">34F</option>
+                      <option value="34G">34G</option>
+                      <option value="34H">34H</option>
+                      <option value="34I">34I</option>
+                      <option value="34J">34J</option>
+                      <option value="36AA">36AA</option>
+                      <option value="36A">36A</option>
+                      <option value="36B">36B</option>
+                      <option value="36C">36C</option>
+                      <option value="36D">36D</option>
+                      <option value="36DD">36DD</option>
+                      <option value="36DDD/E">36E</option>
+                      <option value="36F">36F</option>
+                      <option value="36G">36G</option>
+                      <option value="36H">36H</option>
+                      <option value="36I">36I</option>
+                      <option value="36J">36J</option>
+                      <option value="38AA">38AA</option>
+                      <option value="38A">38A</option>
+                      <option value="38B">38B</option>
+                      <option value="38C">38C</option>
+                      <option value="38D">38D</option>
+                      <option value="38DD">38DD</option>
+                      <option value="38DDD/E">38E</option>
+                      <option value="38F">38F</option>
+                      <option value="38G">38G</option>
+                      <option value="38H">38H</option>
+                      <option value="38I">38I</option>
+                      <option value="38J">38J</option>
+                    </select>
                     <label className="account-item-label">Bra size </label>
                   </div>
                   <div className="account-wrapper-item">
-                    <div className="account-select-round">
-                      <select
-                        value={this.state.size}
-                        onChange={this.handleChange('size')}
-                        name="size"
-                        className="account-select"
-                      >
-                        <option value="0">0</option>
-                        <option value="2">2</option>
-                        <option value="4">4</option>
-                        <option value="6">6</option>
-                        <option value="8">8</option>
-                        <option value="10">10</option>
-                        <option value="12">12</option>
-                        <option value="14">14</option>
-                        <option value="16">16</option>
-                      </select>
-                    </div>
+                    <select
+                      value={this.state.size}
+                      onChange={this.handleChange('size')}
+                      name="size"
+                      className="account-select account-select-sm"
+                    >
+                      <option value="0">0</option>
+                      <option value="2">2</option>
+                      <option value="4">4</option>
+                      <option value="6">6</option>
+                      <option value="8">8</option>
+                      <option value="10">10</option>
+                      <option value="12">12</option>
+                      <option value="14">14</option>
+                      <option value="16">16</option>
+                    </select>
                     <label className="account-item-label">Dress size </label>
                   </div>
                   <div className="account-wrapper-item">
-                    <div className="account-select-round">
-                      <select
-                        value={this.state.waist}
-                        onChange={this.handleChange('waist')}
-                        name="waist"
-                        className="account-select"
-                      >
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        <option value="25">25</option>
-                        <option value="26">26</option>
-                        <option value="27">27</option>
-                        <option value="28">28</option>
-                        <option value="29">29</option>
-                        <option value="30">30</option>
-                        <option value="31">31</option>
-                        <option value="32">32</option>
-                        <option value="33">33</option>
-                        <option value="34">34</option>
-                        <option value="35">35</option>
-                        <option value="36">36</option>
-                      </select>
-                    </div>
+                    <select
+                      value={this.state.waist}
+                      onChange={this.handleChange('waist')}
+                      name="waist"
+                      className="account-select account-select-sm"
+                    >
+                      <option value="23">23</option>
+                      <option value="24">24</option>
+                      <option value="25">25</option>
+                      <option value="26">26</option>
+                      <option value="27">27</option>
+                      <option value="28">28</option>
+                      <option value="29">29</option>
+                      <option value="30">30</option>
+                      <option value="31">31</option>
+                      <option value="32">32</option>
+                      <option value="33">33</option>
+                      <option value="34">34</option>
+                      <option value="35">35</option>
+                      <option value="36">36</option>
+                    </select>
                     <label className="account-item-label">Waist (in.) </label>
                   </div>
                   <div className="account-wrapper-item">
-                    <div className="account-select-round">
-                      <select
-                        value={this.state.hips}
-                        onChange={this.handleChange('hips')}
-                        name="hips"
-                        className="account-select"
-                      >
-                        <option value="32">32</option>
-                        <option value="33">33</option>
-                        <option value="34">34</option>
-                        <option value="35">35</option>
-                        <option value="37">36</option>
-                        <option value="37">37</option>
-                        <option value="38">38</option>
-                        <option value="39">39</option>
-                        <option value="40">40</option>
-                        <option value="40">41</option>
-                        <option value="40">42</option>
-                        <option value="40">43</option>
-                        <option value="40">44</option>
-                        <option value="40">45</option>
-                        <option value="40">46</option>
-                      </select>
-                    </div>
+                    <select
+                      value={this.state.hips}
+                      onChange={this.handleChange('hips')}
+                      name="hips"
+                      className="account-select account-select-sm"
+                    >
+                      <option value="32">32</option>
+                      <option value="33">33</option>
+                      <option value="34">34</option>
+                      <option value="35">35</option>
+                      <option value="37">36</option>
+                      <option value="37">37</option>
+                      <option value="38">38</option>
+                      <option value="39">39</option>
+                      <option value="40">40</option>
+                      <option value="40">41</option>
+                      <option value="40">42</option>
+                      <option value="40">43</option>
+                      <option value="40">44</option>
+                      <option value="40">45</option>
+                      <option value="40">46</option>
+                    </select>
                     <label className="account-item-label">Hips (in.) </label>
                   </div>
                 </div>
